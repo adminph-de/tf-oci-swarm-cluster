7d7f21819da1207cf9635fb6b196c99d2299beb0
adminph-de <36226934+adminph-de@users.noreply.github.com>
2021-04-16 16:06:27 +0200

    added $ to array variable

--- modules/master/scripts/node.lable.sh
+++ modules/master/scripts/node.lable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SWARM_NODES=(`docker node ls --filter role=worker | awk 'NR>1{ print $2}'`)
 echo "Adding Lables to WORKER Nodes:"
-for i in ${!SWARM_NODES[@]}; do
-    WORKER_IP=(`docker node inspect ${SWARM_NODES[$i]} | grep -e '"Addr": ' | awk -F '"' '{ print $4 }'`)
-    WORKER_STATE=(`docker node inspect ${SWARM_NODES[$i]} | grep -e '"State": ' | awk -F '"' '{ print $4 }'`)
+for i in $${!SWARM_NODES[@]}; do
+    WORKER_IP=(`docker node inspect $${SWARM_NODES[$i]} | grep -e '"Addr": ' | awk -F '"' '{ print $4 }'`)
+    WORKER_STATE=(`docker node inspect $${SWARM_NODES[$i]} | grep -e '"State": ' | awk -F '"' '{ print $4 }'`)
     [ $WORKER_STATE == "ready" ] && cat /var/nfsshare/metadata | grep -e "$WORKER_IP" | awk '{ system("docker node update --label-add region="$4 " " $2) }'
     [ $WORKER_STATE == "ready" ] && cat /var/nfsshare/metadata | grep -e "$WORKER_IP" | awk '{ system("docker node update --label-add ad="$5 " " $2) }'
     [ $WORKER_STATE == "ready" ] && cat /var/nfsshare/metadata | grep -e "$WORKER_IP" | awk '{ system("docker node update --label-add size="$6 " " $2) }'
